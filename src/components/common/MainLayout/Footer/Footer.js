0365f8a1381cb69fa427cd8202aec6b6e5aae590
chikanoff <hhappyzor@gmail.com>
2021-11-15 12:23:51 +0300

    add footer

--- src/components/common/MainLayout/Footer/Footer.js
+++ src/components/common/MainLayout/Footer/Footer.js
@@ -1,8 +1,25 @@
-import styled from '@emotion/styled/macro';
 import React from 'react';
+import { Container, Row, Column, FooterLink } from './FooterStyles';
 
-const Root = styled.footer``;
-
-const Footer = () => <Root>footer</Root>;
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Column>
+          <FooterLink href="t.me/chikanoff">Telegram</FooterLink>
+        </Column>
+        <Column>
+          <FooterLink href="vk.com/danyachikanoff">VK</FooterLink>
+        </Column>
+        <Column>
+          <FooterLink href="github.com/chikanoff">Github</FooterLink>
+        </Column>
+        <Column>
+          <FooterLink href="youtube.com">Youtube</FooterLink>
+        </Column>
+      </Row>
+    </Container>
+  );
+};
 
 export default Footer;
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -42,11 +42,11 @@ const rows = [
 
 function DataTable() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: '100%', width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
+        pageSize={8}
         rowsPerPageOptions={[5]}
         checkboxSelection
       />
